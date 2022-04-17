b67de994363e1d3d23ffe3b56b2e3a00013e2747
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-04-17 11:07:46 +0900

    Update main.py

--- Shooting_game/main.py
+++ Shooting_game/main.py
@@ -1,6 +1,8 @@
 import pygame
 
+
 pygame.init() #Game Start
+
 
 win = pygame.display.set_mode((500,480))
 pygame.display.set_caption("Wind's World Game")
@@ -17,6 +19,13 @@ bg = pygame.image.load('source/bg.jpg')
 char = pygame.image.load('source/standing.png')
 
 clock = pygame.time.Clock()
+
+bulletSound = pygame.mixer.Sound("source/bullet.mp3")
+hitSound = pygame.mixer.Sound("source/hit.mp3")
+music = pygame.mixer.music.load("source/music.mp3")
+
+pygame.mixer.music.play(-1) #Infinite Loop the music
+
 
 class Projectile(object):
     def __init__(self, x, y, radius, color, facing):
@@ -66,6 +75,25 @@ class Player(object):
         self.hitbox = (self.x + 17, self.y + 11, 29, 52)
         pygame.draw.rect(win, (255,0,0), self.hitbox, 2) #2 = borderness
 
+    def hit(self):
+        self.x = 60
+        self.y = 410
+        self.walkCount = 0
+        font1 = pygame.font.SysFont('comicsans', 100)
+        text = font1.render('-10', 1, (255,0,0))
+        win.blit(text, (250 - (text.get_width()/2), 200))
+        pygame.display.update()
+
+        i = 0
+        while i < 300:
+            pygame.time.delay(10)
+            i += 1
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    i = 301
+                    pygame.quit()
+
+
 class Enenmy(object):
     walkRight = [pygame.image.load('source/R1E.png'), pygame.image.load('source/R2E.png'), pygame.image.load('source/R3E.png'), 
     pygame.image.load('source/R4E.png'), pygame.image.load('source/R5E.png'), pygame.image.load('source/R6E.png'), 
@@ -86,6 +114,8 @@ class Enenmy(object):
         self.walkCount = 0
         self.velocity = 3
         self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+        self.health = 50
+        self.visible = True
     
     def move(self):
         if self.velocity > 0: #if right
@@ -105,19 +135,29 @@ class Enenmy(object):
 
     def draw(self, win):
         self.move()
-        if self.walkCount + 1>= 33:
-            self.walkCount = 0 
-        if self.velocity > 0:
-            win.blit(self.walkRight[self.walkCount//3], (self.x,self.y))
-            self.walkCount += 1
-        else:
-            win.blit(self.walkLeft[self.walkCount//3], (self.x,self.y))
-            self.walkCount += 1
-        self.hitbox = (self.x + 17, self.y + 2, 31, 57)
-        pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
+        if self.visible:
+            if self.walkCount + 1>= 33:
+                self.walkCount = 0 
+            if self.velocity > 0:
+                win.blit(self.walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+            else:
+                win.blit(self.walkLeft[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+            #Health Bar
+            pygame.draw.rect(win, (255,0,0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10))
+            #Health Bar Background
+            pygame.draw.rect(win, (0,128,0), (self.hitbox[0], self.hitbox[1] - 20, (50 - self.health), 10))
+
+            self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+            pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
 
     def hit(self):
-        print("Hit!")
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.visible = False
+        print("hit")
 
 def redrawGameWindow():
     win.blit(bg, (0,0))
@@ -125,6 +165,8 @@ def redrawGameWindow():
     goblin.draw(win)
     for bullet in bullets:
         bullet.draw(win)
+    text = font.render("Score: " + str(score), 1, (0,0,0))
+    win.blit(text, (320,10))
     pygame.display.update()
 
 haejeok = Player(200, 410, 64, 64)
@@ -132,9 +174,16 @@ goblin = Enenmy(100, 410, 64, 64, 300)
 bullets = []
 run = True
 shootLoop = 0
+score = 0
+font = pygame.font.SysFont("comicsans", 30, True)
 
 while run:
     clock.tick(27)
+
+    if haejeok.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and haejeok.hitbox[1] + haejeok.hitbox[3] > goblin.hitbox[1]:
+        if haejeok.hitbox[0] + haejeok[2] > goblin.hitbox[0] and haejeok.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
+            haejeok.hit()
+            score -= 10
 
     if shootLoop > 0:
         shootLoop += 1
@@ -148,7 +197,9 @@ while run:
     for bullet in bullets:
         if bullet.y - bullet.radius < goblin.hitbox[1] + goblin.hitbox[3] and bullet.y + bullet.radius > goblin.hitbox[1]:
             if bullet.x + bullet.radius > goblin.hitbox[0] and bullet.x - bullet.radius < goblin.hitbox[0] + goblin.hitbox[2]:
+                hitSound.play()
                 goblin.hit()
+                score += 1
                 bullets.pop(bullets.index(bullet))
 
         if bullet.x < 500 and bullet.x > 0:
@@ -160,6 +211,7 @@ while run:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a] and shootLoop == 0:
+        bulletSound.play()
         if haejeok.left:
             facing = -1
         else:
